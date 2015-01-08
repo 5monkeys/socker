1b8cba9a97561d698d84f1564b98c1089ba8288d
Jonas Lundberg <jonas@5monkeys.se>
2015-01-08 09:33:06 +0100

    Add signal watching to cli command

--- src/socker/cli/command.py
+++ src/socker/cli/command.py
@@ -16,6 +16,7 @@ Options:
 
 """
 import logging
+import signal
 from docopt import docopt
 
 from . import log
@@ -25,25 +26,44 @@ logger = logging.getLogger(__name__)
 
 
 class Interface(object):
+
     def __init__(self):
         self.opts = docopt(__doc__, version='socker v{}'.format(__version__))
-
         self.setup_logging()
-
-        try:
-            self.run()
-        except KeyboardInterrupt:
-            self.quit()
-
-    def run(self):
-        server.main(
-            interface=self.opts['-i'],
-            port=int(self.opts['-p']))
+        self.register_signals()
+        self.start()
 
     def setup_logging(self):
         filename = self.opts['--logto']
         verbose = self.opts['-v']
         log.configure(filename, verbose)
+
+    def register_signals(self):
+        signal.signal(signal.SIGHUP, lambda *args: self.reload())  # 1; Reload
+        signal.signal(signal.SIGINT, lambda *args: self.abort())   # 2; Interrupt, ctrl-c
+        signal.signal(signal.SIGTERM, lambda *args: self.stop())   # 15; Stop
+
+    def start(self):
+        server.main(
+            interface=self.opts['-i'],
+            port=int(self.opts['-p']))
+
+    def reload(self):
+        logger.warn('--- SIGHUP ---')
+
+    def abort(self):
+        logger.warn('--- SIGINT ---')
+        self.safe_quit()
+
+    def stop(self):
+        logger.warn('--- SIGTERM ---')
+        self.safe_quit()
+
+    def safe_quit(self):
+        # TODO: Implement safer way to exit
+        # TODO: Break inner loops with something like "while not quit:" or custom exceptions
+        # self._quit = True
+        self.quit()
 
     def quit(self):
         logger.info('Bye!')
