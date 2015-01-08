fa7ca3d861a2a970c3e8198f277ea4549f163b42
Jonas Lundberg <jonas@5monkeys.se>
2015-01-08 17:27:13 +0100

    Lazy load message data/json

--- src/socker/transport.py
+++ src/socker/transport.py
@@ -4,7 +4,14 @@ import json
 class SockMessage:
     def __init__(self, name, data):
         self.name = name
-        self.data = data
+        self._data = data
+
+    @property
+    def data(self):
+        if isinstance(self._data, (bytes, str)):
+            self._data = json.loads(self._data)
+
+        return self._data
 
     @classmethod
     def from_redis(cls, reply):
@@ -12,9 +19,7 @@ class SockMessage:
 
     @classmethod
     def from_string(cls, string):
-        name, json_str = string.split('|', 1)
-        data = json.loads(json_str)
-
+        name, data = string.split('|', 1)
         return cls(name, data)
 
     def __str__(self):
